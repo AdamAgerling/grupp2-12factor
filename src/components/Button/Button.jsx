aba1d9c4528092346592d21c2e72afcd5dbeae1e
Ludwig Nojd <ludwig.nojd@hotmail.se>
2022-10-31 13:36:38 +0100

    Chnagas

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,5 @@
-import styles from './Button.module.css'
+import styles from "./Button.module.css"
 
-export const Button = ({children}) => {
-  return (
-    <button className={`${styles.button}`}>{children}</button>
-  )
+export const Button = ({ children }) => {
+  return <button className={`${styles.button}`}>{children}</button>
 }
